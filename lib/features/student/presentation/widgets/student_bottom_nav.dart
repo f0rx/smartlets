7b71ee39bcd03103268896a0e00151735234d617
Brendan Ejike <ejike.br@gmail.com>
2020-09-27 20:39:10 +0100

    Implement Curved Bottom Nav (Student section)

--- lib/features/student/presentation/widgets/student_bottom_nav.dart
+++ lib/features/student/presentation/widgets/student_bottom_nav.dart
@@ -1,44 +1,75 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smartlets/features/auth/domain/core/auth.dart';
+import 'package:smartlets/features/shared/domain/entities/user/user.dart';
 import 'package:smartlets/features/student/domain/domain.dart';
 import 'package:smartlets/features/student/presentation/manager/blocs.dart';
+import 'package:smartlets/manager/locator/locator.dart';
 import 'package:smartlets/utils/utils.dart';
 
 class StudentBottomNav extends StatelessWidget {
-  final List<StudentDestination<Widget>> destinations;
+  final List<StudentDestination<IconData>> destinations;
 
   const StudentBottomNav(this.destinations, {Key key}) : super(key: key);
+
+  Image _image(String image) {
+    if (image != null) return Image.network(image);
+    return Image.asset("${AppAssets.HOME_IMAGES_DIR}/default-user.png");
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<StudentNavCubit, int>(
-      builder: (context, state) => BottomNavigationBar(
-        currentIndex: state,
-        onTap: (index) {
-          if (state != index)
-            context.bloc<StudentNavCubit>().indexChanged(index);
-          // If the user is re-selecting the tab, the common
-          // behavior is to empty the stack.
-          else
-            inner(context).popUntil((route) => route.isFirst);
+      builder: (context, state) => StreamBuilder<Option<User>>(
+        stream: getIt<AuthFacade>().onAuthStateChanged,
+        builder: (context, snapshot) {
+          return CurvedNavigationBar(
+            index: state,
+            height: kBottomNavigationBarHeight,
+            items: destinations
+                .map(
+                  (dest) => Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Visibility(
+                        visible: !dest.title.caseInsensitiveContains("profile"),
+                        replacement: CircleAvatar(
+                          backgroundImage: _image(snapshot.data?.getOrElse(() => null)?.photoURL).image,
+                          backgroundColor: Theme.of(context).accentColor,
+                          radius: 14,
+                        ),
+                        child: Icon(
+                          dest.selectedIcon,
+                          color: Theme.of(context).accentColor,
+                          semanticLabel: "${dest.title}",
+                        ),
+                      ),
+                      Visibility(
+                        visible: false,
+                        child: Text(dest.title),
+                      ),
+                    ],
+                  ),
+                )
+                .toList(),
+            color: Theme.of(context).primaryColor,
+            buttonBackgroundColor: Theme.of(context).primaryColor,
+            backgroundColor: Colors.transparent,
+            animationCurve: Curves.easeInOut,
+            animationDuration: Duration(milliseconds: 500),
+            onTap: (index) {
+              if (state != index)
+                context.bloc<StudentNavCubit>().indexChanged(index);
+              // If the user is re-selecting the tab, the common
+              // behavior is to empty the stack.
+              else
+                inner(context).popUntil((route) => route.isFirst);
+            },
+          );
         },
-        elevation: 3.0,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        selectedItemColor: AppColors.accentColor.shade600,
-        unselectedItemColor: Helpers.optionOf(AppColors.secondaryColor.shade400, AppColors.primaryColor),
-        items: destinations
-            .map<BottomNavigationBarItem>(
-              (e) => BottomNavigationBarItem(
-                title: Text(e.title),
-                icon: e.unSelectedIcon,
-                activeIcon: e.selectedIcon,
-              ),
-            )
-            .toList(),
       ),
     );
   }
