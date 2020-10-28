66bf6fd746b21beb92c43270a68633678e384d41
Brendan Ejike <ejike.br@gmail.com>
2020-10-28 12:40:48 +0100

    Fix splash screen bug

--- lib/features/on_boarding/presentation/splash_screen.dart
+++ lib/features/on_boarding/presentation/splash_screen.dart
@@ -14,51 +14,53 @@ import 'package:smartlets/widgets/widgets.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Option<User>>(
-      future: Future.delayed(env.splashDuration, () => getIt<AuthFacade>().currentUser),
-      builder: (_, snapshot) {
-        if (snapshot.hasData)
-          getIt<AuthFacade>().onAuthStateChanged?.listen((option) => option?.fold(
-                () => navigator.pushAndRemoveUntil(Routes.onBoardingScreen, (route) => false),
-                // Using this screens' context is wrong because it will be disposed on navigation; hence the use of App.context
-                (_) => BlocProvider.of<OnBoardingCubit>(App.context).state.subscription?.fold(
-                      parent: () => navigator.pushAndRemoveUntil(Routes.parentRootScreen, (route) => false),
-                      student: () => navigator.pushAndRemoveUntil(Routes.studentRootScreen, (route) => false),
-                    ),
-              ));
+    return BlocBuilder<OnBoardingCubit, OnBoardingState>(
+      builder: (context, state) => FutureBuilder<Option<User>>(
+        future: Future.delayed(env.splashDuration, () => getIt<AuthFacade>().currentUser),
+        builder: (_, snapshot) {
+          if (snapshot.hasData)
+            getIt<AuthFacade>().onAuthStateChanged?.listen((option) => option?.fold(
+                  () => navigator.pushAndRemoveUntil(Routes.onBoardingScreen, (route) => false),
+                  // Using this screens' context is wrong because it will be disposed on navigation; hence the use of App.context
+                  (_) => BlocProvider.of<OnBoardingCubit>(App.context).state.subscription?.fold(
+                        parent: () => navigator.pushAndRemoveUntil(Routes.parentRootScreen, (route) => false),
+                        student: () => navigator.pushAndRemoveUntil(Routes.studentRootScreen, (route) => false),
+                      ),
+                ));
 
-        return Scaffold(
-          backgroundColor: Theme.of(context).accentColor,
-          body: Stack(
-            alignment: Alignment.center,
-            clipBehavior: Clip.antiAliasWithSaveLayer,
-            children: [
-              Positioned(
-                top: App.height * 0.45,
-                left: 0,
-                right: 0,
-                child: Align(
-                  alignment: Alignment.center,
-                  child: AutoSizeText(
-                    AppStrings.appName,
-                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
-                    minFontSize: 40,
+          return Scaffold(
+            backgroundColor: Theme.of(context).accentColor,
+            body: Stack(
+              alignment: Alignment.center,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              children: [
+                Positioned(
+                  top: App.height * 0.45,
+                  left: 0,
+                  right: 0,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: AutoSizeText(
+                      AppStrings.appName,
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+                      minFontSize: 40,
+                    ),
                   ),
                 ),
-              ),
-              Positioned(
-                bottom: App.height * 0.02,
-                child: CircularProgressBar.adaptive(
-                  width: App.width * 0.09,
-                  height: App.width * 0.09,
-                  background: Colors.white,
-                  strokeWidth: 2.5,
+                Positioned(
+                  bottom: App.height * 0.02,
+                  child: CircularProgressBar.adaptive(
+                    width: App.width * 0.09,
+                    height: App.width * 0.09,
+                    background: Colors.white,
+                    strokeWidth: 2.5,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        );
-      },
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
